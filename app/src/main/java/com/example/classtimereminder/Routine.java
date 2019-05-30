861b0c4893857ed0162eb9fa66bfd6a877dd7937
FahimShahriar <fahims.cse.1997@gmail.com>
2019-05-30 13:19:26 +0600

    intial UI design

---
+++ app/src/main/java/com/example/classtimereminder/Routine.java
@@ -0,0 +1,13 @@
+package com.example.classtimereminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Routine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routine);
+    }
+}
