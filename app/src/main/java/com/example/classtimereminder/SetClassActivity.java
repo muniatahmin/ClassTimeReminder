f7a8ade66986acb95678d15fa9d2429dac3ceac5
muniatahmin <munia.tahmin.cse@gmail.com>
2019-07-03 01:09:25 +0600

    Added a new activity, and designed

--- app/src/main/java/com/example/classtimereminder/SetClassActivity.java
+++ app/src/main/java/com/example/classtimereminder/SetClassActivity.java
@@ -2,6 +2,7 @@ package com.example.classtimereminder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SetClassActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class SetClassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_class);
+    }
+
+    public void onClickDone(View v)
+    {
+        ;
     }
 }
